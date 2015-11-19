5536181cd739bc7cd8b4aaafbf0d826c1f3808ec
Christian de Barrio Arribas <christiandebarrio@gmail.com>
2015-11-19 17:30:15 +0100

    validates user name and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+
+  validates :name, presence: true
+  validates :name, length: { maximum: 30 }
+  validates :email, presence: true
+  validates :email, format: { with: /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/ }
+
 end
