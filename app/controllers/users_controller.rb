58521858c544c2e462d456e68331802ec7546652
Christian de Barrio Arribas <christiandebarrio@gmail.com>
2015-11-20 09:49:16 +0100

    delete in process

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,12 @@ class UsersController < ApplicationController
     @user = User.find_by(id: params[:id])
   end
 
+  def destroy
+    @user = User.find_by(id: params[:id])
+    @user.destroy
+    redirect_to action: 'home', controller: 'site'
+  end
+
   private
 
   def user_params
